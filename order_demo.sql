1b1206235d374575e8bd198c87d3d799d9697fd6
unknown <qq1132416@outlook.com>
2019-08-11 14:16:19 +0800

    分页查询优化及其例子

---
+++ order_demo.sql
@@ -0,0 +1,21 @@
+create table ORDER_DMEO (
+	id int(11) not null primary key auto_increment,
+	order_no varchar(36),
+	receiver varchar(24),
+	sender varchar(24)
+);
+alter table ORDER_DMEO add index order_no_index(order_no)
+
+drop procedure idata;
+delimiter ;;
+create procedure idata()
+begin
+  declare i int;
+  set i=1;
+  while(i<=12000)do
+    insert into ORDER_DMEO(order_no,receiver,sender) values(i, i, i);
+    set i=i+1;
+  end while;
+end;;
+delimiter ;
+call idata();

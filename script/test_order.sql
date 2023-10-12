02c04048aa00460e03e5b8e2296a0b3539b9c648
worldluoji <qq1132416@outlook.com>
2023-10-12 10:29:02 +0800

    docs: delete test_order.sql

--- script/test_order.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `t` (
-  `id` int(11) NOT NULL,
-  `city` varchar(16) NOT NULL,
-  `name` varchar(16) NOT NULL,
-  `age` int(11) NOT NULL,
-  `addr` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `city` (`city`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
